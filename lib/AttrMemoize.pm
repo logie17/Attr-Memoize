0975145fdcd5146f6589c3047966c6c426822cd1
Logan Bell <loganbell@gmail.com>
2014-03-31 12:43:28 -0700

    Library

---
+++ lib/AttrMemoize.pm
@@ -0,0 +1,19 @@
+use strict;
+use warnings;
+package AttrMemoize;
+
+use Attribute::Handlers;
+
+my $cache = {};
+
+sub Memoize :ATTR {
+    my ($package, $symbol, $referent) = @_;
+
+    my $name = *{$symbol}{NAME};
+    no strict 'refs';
+    *{"$package\::$name"} = sub {
+	$cache->{$package} ||= $referent->(@_);
+    };
+}
+
+1;
